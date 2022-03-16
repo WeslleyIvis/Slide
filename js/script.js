0e4c783e30e1bebe3c0df6f497f4f14afbf14c50
WeslleyIvis <weslley_reive@hotmail.com>
2022-03-16 19:34:40 -0300

    funcionalidades no carrousel de next e prev

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ import Slide from './slide.js'
 const slide = new Slide('.slide', '.slide-wrapper')
 slide.init();
 
-slide.changeSlide(4)
+console.log(slide);
+
+slide.changeSlide(3);
+slide.activePrevSlide();
